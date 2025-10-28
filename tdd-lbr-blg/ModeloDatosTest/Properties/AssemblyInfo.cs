1fc03e5ec4787e8832de2f1281892d4786e7ecb4
lbr1014 <lbr1014@alu.ubu.es>
2025-10-28 14:56:44 +0100

    Subir proyecto al repositorio

---
+++ tdd-lbr-blg/ModeloDatosTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ModeloDatosTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("ModeloDatosTest")]
+[assembly: AssemblyCopyright("Copyright © HP 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("616566d6-aa3e-40ce-9014-afe1e5af85d9")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ tdd-lbr-blg/ModeloDatosTest/UsuarioTest.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModeladoDatos;
+using System;
+
+namespace ModeloDatosTest
+{
+    [TestClass]
+    public class UsuarioTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            int idUsuario = 1;
+            string nombre = "Lydia";
+            string email = "lbr1014@alu.ubu.es";
+            string password = "ConMasDe12Caracteres!";
+            Usuario u = new Usuario(idUsuario, nombre, email, password);
+
+            Assert.Equals(u.idUsuario, idUsuario);
+
+            Assert.IsFalse(u.ComprobarContraseña("password"));
+            Assert.IsTrue(u.ComprobarContraseña("ConMasDe12Caracteres!"));
+
+            Assert.IsFalse(u.CambiarContraseña("anterior", "mueva"));
+        }
+    }
+}
