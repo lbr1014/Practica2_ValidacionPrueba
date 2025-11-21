5dfeff63283db5c519403c9efa3687ba0c7ab353
lbr1014 <lbr1014@alu.ubu.es>
2025-11-22 00:57:09 +0100

    Clase rol acabada

---
+++ tdd-lbr-blg/ModeloDatos/Rol.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+
+namespace ModeloDatos
+{
+    public class Rol
+    {
+        private int id;
+        private string nombre;
+        private string descripcion;
+        private bool edicionPlanDePruebas;
+        private bool ejecucionPlanDePrueba;
+        private bool ejecucionCasosPruebas;
+        private bool edicionCasosPruebas;
+        private bool gestiones;
+
+        public Rol(int id, string nombre, string descripcion, bool edicionPlanDePruebas, bool ejecucionPlanDePrueba, bool ejecucionCasosPruebas, bool edicionCasosPruebas, bool gestiones)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+            this.edicionPlanDePruebas = edicionPlanDePruebas;
+            this.ejecucionPlanDePrueba = ejecucionPlanDePrueba;
+            this.ejecucionCasosPruebas = ejecucionCasosPruebas;
+            this.edicionCasosPruebas = edicionCasosPruebas;
+            this.gestiones = gestiones;
+        }
+
+        public int Id {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+        public string Nombre { 
+            get { return this.nombre; }
+            set { this.nombre = value; }
+        }
+        public string Descripcion {
+            get { return this.descripcion; }
+            set { this.descripcion = value; }
+        }
+        public bool EdicionPlanDePruebas {
+            get { return this.edicionPlanDePruebas; }
+            set { this.edicionPlanDePruebas = value; }
+        }
+        public bool EjecucionPlanDePrueba {
+            get { return this.ejecucionPlanDePrueba; }
+            set { this.ejecucionPlanDePrueba = value; }
+        }
+        public bool EjecucionCasosPruebas {
+            get { return this.ejecucionCasosPruebas; }
+            set { this.ejecucionCasosPruebas = value; }
+        }
+        public bool EdicionCasosPruebas {
+            get { return this.edicionCasosPruebas; }
+            set { this.edicionCasosPruebas = value; }
+        }
+        public bool Gestiones {
+            get { return this.gestiones; }
+            set { this.gestiones = value; }
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Rol rol &&
+                   id == rol.id &&
+                   nombre == rol.nombre &&
+                   descripcion == rol.descripcion &&
+                   edicionPlanDePruebas == rol.edicionPlanDePruebas &&
+                   ejecucionPlanDePrueba == rol.ejecucionPlanDePrueba &&
+                   ejecucionCasosPruebas == rol.ejecucionCasosPruebas &&
+                   edicionCasosPruebas == rol.edicionCasosPruebas &&
+                   gestiones == rol.gestiones &&
+                   Id == rol.Id &&
+                   Nombre == rol.Nombre &&
+                   Descripcion == rol.Descripcion &&
+                   EdicionPlanDePruebas == rol.EdicionPlanDePruebas &&
+                   EjecucionPlanDePrueba == rol.EjecucionPlanDePrueba &&
+                   EjecucionCasosPruebas == rol.EjecucionCasosPruebas &&
+                   EdicionCasosPruebas == rol.EdicionCasosPruebas &&
+                   Gestiones == rol.Gestiones;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -1030890594;
+            hashCode = hashCode * -1521134295 + id.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(nombre);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(descripcion);
+            hashCode = hashCode * -1521134295 + edicionPlanDePruebas.GetHashCode();
+            hashCode = hashCode * -1521134295 + ejecucionPlanDePrueba.GetHashCode();
+            hashCode = hashCode * -1521134295 + ejecucionCasosPruebas.GetHashCode();
+            hashCode = hashCode * -1521134295 + edicionCasosPruebas.GetHashCode();
+            hashCode = hashCode * -1521134295 + gestiones.GetHashCode();
+            hashCode = hashCode * -1521134295 + Id.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Nombre);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Descripcion);
+            hashCode = hashCode * -1521134295 + EdicionPlanDePruebas.GetHashCode();
+            hashCode = hashCode * -1521134295 + EjecucionPlanDePrueba.GetHashCode();
+            hashCode = hashCode * -1521134295 + EjecucionCasosPruebas.GetHashCode();
+            hashCode = hashCode * -1521134295 + EdicionCasosPruebas.GetHashCode();
+            hashCode = hashCode * -1521134295 + Gestiones.GetHashCode();
+            return hashCode;
+        }
+    }
+}
--- tdd-lbr-blg/ModeloDatosTest/RolTest.cs
+++ tdd-lbr-blg/ModeloDatosTest/RolTest.cs
@@ -21,11 +21,15 @@ namespace ModeloDatosTest
         bool gestiones = true;
 
         Rol r = null;
+        Rol r2 = null;
+        Rol r3 = null;
 
         [TestInitialize]
         public void InicializaTest()
         {
             r = new Rol(id, nombre, descripcion, edicionPlanDePruebas, ejecucionPlanDePrueba, ejecucionCasosPruebas, edicionCasosPruebas, gestiones);
+            r2 = new Rol(id, nombre, descripcion, edicionPlanDePruebas, ejecucionPlanDePrueba, ejecucionCasosPruebas, edicionCasosPruebas, gestiones);
+            r3 = new Rol(1, "tester", "rol encargado de realizar las pruebas", true, true, true, false, false);
 
         }
 
@@ -34,14 +38,63 @@ namespace ModeloDatosTest
         {
             Assert.IsNotNull(r);
 
-            Assert.AreEqual(id, r.id);
+            Assert.AreEqual(id, r.Id);
             Assert.AreEqual(nombre, r.Nombre);
             Assert.AreEqual(descripcion, r.Descripcion);
-            Assert.AreEqual(edicionPlanDePruebas, r.edicionPlanDePruebas);
-            Assert.AreEqual(ejecucionPlanDePrueba, r.ejecucionPlanDePrueba);
-            Assert.AreEqual(ejecucionCasosPruebas, r.ejecucionCasosPruebas);
-            Assert.AreEqual(edicionCasosPruebas, r.edicionCasosPruebas);
-            Assert.AreEqual(gestiones, r.gestiones);
+            Assert.AreEqual(edicionPlanDePruebas, r.EdicionPlanDePruebas);
+            Assert.AreEqual(ejecucionPlanDePrueba, r.EjecucionPlanDePrueba);
+            Assert.AreEqual(ejecucionCasosPruebas, r.EjecucionCasosPruebas);
+            Assert.AreEqual(edicionCasosPruebas, r.EdicionCasosPruebas);
+            Assert.AreEqual(gestiones, r.Gestiones);
+        }
+
+        [TestMethod]
+        public void GetYSetTest()
+        {
+            r.Id = 33;
+            Assert.AreEqual(33, r.Id);
+
+            r.Nombre = "Tester";
+            Assert.AreEqual("Tester", r.Nombre);
+
+            r.Descripcion = "rol encargado de realizar las pruebas";
+            Assert.AreEqual("rol encargado de realizar las pruebas", r.Descripcion);
+
+            r.EdicionPlanDePruebas = false;
+            Assert.AreEqual(false, r.EdicionPlanDePruebas);
+
+            r.EjecucionPlanDePrueba = false;
+            Assert.AreEqual(false, r.EjecucionPlanDePrueba);
+
+            r.EjecucionCasosPruebas = false;
+            Assert.AreEqual(false, r.EjecucionCasosPruebas);
+
+            r.EdicionCasosPruebas = false;
+            Assert.AreEqual(false, r.EdicionCasosPruebas);
+
+            r.Gestiones = false;
+            Assert.AreEqual(false, r.Gestiones);
+
+        }
+
+        [TestMethod()]
+        public void EqualsTest()
+        {
+            Assert.IsTrue(r.Equals(r2));
+            Assert.IsFalse(r.Equals(r3));
+
+        }
+
+        [TestMethod()]
+        public void GetHashCodeTest()
+        {
+
+            int hash1 = r.GetHashCode();
+            int hash2 = r2.GetHashCode();
+            Assert.AreEqual(hash1, hash2);
+
+            int hash3 = r3.GetHashCode();
+            Assert.AreNotEqual(hash1, hash3);
         }
 
 
