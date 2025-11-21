b5889ebf92376d4c436883b876748e842b418cba
lbr1014 <lbr1014@alu.ubu.es>
2025-11-21 12:55:18 +0100

    Test clase usuario, principio de la clase y capturas de esta parte

---
+++ tdd-lbr-blg/ModeloDatos/Usuario.cs
@@ -0,0 +1,130 @@
+﻿using System;
+
+namespace ModeloDatos
+{
+    public class Usuario
+    {
+        private int id;
+        private string nombre;
+        private string apellidos;
+        private string direccion;
+        private int codigoPostal;
+        private string email;
+        private string contraseña;
+        private DateTime fechaCaducidadCuenta;
+        private DateTime fechaCaducidadContraseña;
+        private DateTime ultimoAcceso;
+        private bool estado;
+
+        public Usuario(int id, string nombre, string apellidos, string direccion, int codigoPostal, string email, string contraseña, DateTime fechaCaducidadCuenta, DateTime fechaCaducidadContraseña, DateTime ultimoAcceso, bool estado)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.apellidos = apellidos;
+            this.direccion = direccion;
+            this.codigoPostal = codigoPostal;
+            this.email = email;
+            this.contraseña = contraseña;
+            this.fechaCaducidadCuenta = fechaCaducidadCuenta;
+            this.fechaCaducidadContraseña = fechaCaducidadContraseña;
+            this.ultimoAcceso = ultimoAcceso;
+            this.estado = estado;
+        }
+
+        public bool ValidarContraseña(string password)
+        {
+            /*
+             * Este método valida una contraseña, obligando a que tenga más de 12 caracteres, al menos
+             * 2 mayusculas, 2 números y 1 caracter especial.
+             * Parametros:
+             *      password: contraseña sin cifrar.
+             * Returns:
+             *      true si cumple las condiciones; false en caso contrario.
+             */
+            int contadorMayusculas = 0;
+            int contadorMinusculas = 0;
+            int contadorNumeros = 0;
+            int contadorCaracteresEspeciales = 0;
+
+            if (string.IsNullOrWhiteSpace(password) || password.Length < 12)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < password.Length; i++)
+            {
+                char caracterContrasena = password[i];
+
+                if (char.IsUpper(caracterContrasena))
+                {
+                    contadorMayusculas++;
+
+                }
+                else if (char.IsLower(caracterContrasena))
+                {
+                    contadorMinusculas++;
+
+                }
+                else if (char.IsDigit(caracterContrasena))
+                {
+                    contadorNumeros++;
+
+                }
+                else
+                {
+                    contadorCaracteresEspeciales++;
+                }
+            }
+
+            return contadorMayusculas >= 2 && contadorMinusculas >= 2 && contadorNumeros >= 1 && contadorCaracteresEspeciales >= 1;
+        }
+
+
+        /* GETS Y SETS */
+        public int Id { 
+            get { return this.id; }
+            set { this.id = value; } 
+        }
+        public string Nombre {
+            get { return this.nombre; }
+            set { this.nombre = value; }
+        }
+        public string Apellidos {
+            get { return this.apellidos; }
+            set { this.apellidos = value; }
+ 
+        }
+        public string Direccion {
+            get { return this.direccion; }
+            set { this.direccion = value; } 
+        }
+        public int CodigoPostal {
+            get { return this.codigoPostal; }
+            set { this.codigoPostal = value; }
+ 
+        }
+        public string Email {
+            get { return this.email; }
+            set { this.email = value; }
+
+        }
+        public DateTime FechaCaducidadCuenta {
+            get { return this.fechaCaducidadCuenta; }
+            set { this.fechaCaducidadCuenta = value; }
+        }
+        public DateTime FechaCaducidadContraseña {
+            get { return this.fechaCaducidadContraseña; }
+            set { this.fechaCaducidadContraseña = value; }
+
+        }
+        public DateTime UltimoAcceso {
+            get { return this.ultimoAcceso; }
+            set { this.ultimoAcceso = value; }
+
+        }
+        public bool Estado {
+            get { return this.estado; }
+            set { this.estado = value; }
+        }
+    }
+}
--- tdd-lbr-blg/ModeloDatosTest/UsuarioTest.cs
+++ tdd-lbr-blg/ModeloDatosTest/UsuarioTest.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ModeloDatos;
+using RestSharp;
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace ModeloDatosTest
 {
@@ -9,18 +14,27 @@ namespace ModeloDatosTest
     public class UsuarioTest
     {
         int id = 0;
-        string nombre = "Lydia";
-        string apellidos = "Blanco Ruiz";
-        string direccionPostal = "C/ COndesa Mencia 120";
-        string email = "lbr1014@alu.ubu.es";
-        string password = "ConMasDe12Caracteres!";
-        DateTime hoy = DateTime.Now;
+        string nombre = "Alexia";
+        string apellidos = "Putellas Segura";
+        string direccion = "C/ Carrer del Mig 5";
+        int codigoPostal = 08970;
+        string email = "balonDeOro3@gmail.com";
+        string contraseña = "ConMasDe12Caracteres!";
+        DateTime fechaCaducidadCuenta = DateTime.Now.AddDays(365);
+        DateTime fechaCaducidadContraseña = DateTime.Now.AddDays(365);
+        DateTime ultimoAcceso = DateTime.MinValue;
+        bool estado = true;
+
         Usuario u = null;
+        Usuario u2 = null; 
+        Usuario u3 = null;
 
         [TestInitialize]
         public void InicializaTest()
         {
-            u = new Usuario(id, nombre, apellidos, email, password);
+            u = new Usuario(id, nombre, apellidos, direccion, codigoPostal, email, contraseña, fechaCaducidadCuenta, fechaCaducidadContraseña, ultimoAcceso, estado);
+            u2 = new Usuario(id, nombre, apellidos, direccion, codigoPostal, email, contraseña, fechaCaducidadCuenta, fechaCaducidadContraseña, ultimoAcceso, estado);
+            u3 = new Usuario(1, "Andrea", "Medina Martín", "C/ Alcalá de Enares", 28805, "andreMedina@gmail.com", "ConMasDe12Caracteres!", fechaCaducidadCuenta, fechaCaducidadContraseña, ultimoAcceso, estado);
 
         }
         [TestMethod]
@@ -28,23 +42,28 @@ namespace ModeloDatosTest
         {
             
             Assert.IsNotNull(u);
+
             Assert.AreEqual(id, u.Id);
             Assert.AreEqual(nombre, u.Nombre);
             Assert.AreEqual(apellidos, u.Apellidos);
-            Assert.AreEqual(direccionPostal, u.DireccionPostal);
-            Assert.AreEqual(email, u.email);
+            Assert.AreEqual(direccion, u.Direccion);
+            Assert.AreEqual(codigoPostal, u.CodigoPostal);
+            Assert.AreEqual(email, u.Email);
+            Assert.AreEqual(fechaCaducidadCuenta, u.FechaCaducidadCuenta);
+            Assert.AreEqual(fechaCaducidadContraseña, u.FechaCaducidadContraseña);
+            Assert.AreEqual(ultimoAcceso, u.UltimoAcceso);
+            Assert.AreEqual(estado, u.Estado);
+
             Assert.IsTrue(u.CuentaActiva);
-            Assert.IsTrue(u.ComprobarContraseña(password));
+            Assert.IsTrue(u.ComprobarContraseña(contraseña));
+            Assert.IsTrue(u.ValidarContraseña(contraseña));
             Assert.AreEqual(DateTime.MinValue, u.UltimoAceso);
 
             //Comprobamos las fechas
+            DateTime hoy = DateTime.Now;
             Assert.AreEqual(hoy.AddDays(365), u.FechaCaducidadCuenta);
             Assert.AreEqual(hoy.AddDays(365), u.FechaCaducidadContraseña);
 
-
-
-            Assert.IsTrue(u.ComprobarContraseña("ConMasDe12Caracteres!"));
-            Assert.IsFalse(u.CambiarContraseña("anterior", "mueva"));
         }
 
         [TestMethod]
@@ -52,7 +71,167 @@ namespace ModeloDatosTest
         {
             u.Id = 33;
             Assert.AreEqual(33, u.Id);
+
+            u.Nombre = "Lando";
+            Assert.AreEqual("Lando", u.Nombre);
+
+            u.Apellidos = "Norris";
+            Assert.AreEqual("Norris", u.Apellidos);
+
+            u.Direccion = "C/Bd Albert 1";
+            Assert.AreEqual("C/Bd Albert 1", u.Direccion);
+
+            u.CodigoPostal = 98000;
+            Assert.AreEqual(98000, u.CodigoPostal);
+
+            u.Email = "primerMundial@gmail.com";
+            Assert.AreEqual("primerMundial@gmail.com", u.Email);
+
+            DateTime nuevaFecha = DateTime.Now.AddDays(15);
+            u.FechaCaducidadCuenta= nuevaFecha;
+            Assert.AreEqual(nuevaFecha, u.FechaCaducidadCuenta);
+
+            u.FechaCaducidadContraseña=nuevaFecha;
+            Assert.AreEqual(nuevaFecha, u.FechaCaducidadContraseña);
+
+            nuevaFecha = DateTime.Now.AddDays(-15);
+            u.UltimoAcceso= nuevaFecha;
+            Assert.AreEqual(nuevaFecha, u.UltimoAcceso);
+
+            u.Estado = false;
+            Assert.IsFalse(u.Estado);
+
         }
+
+        public static IEnumerable<object[]> ObtenerContraseñaDesdeJson()
+        {
+            string filePath = Path.Combine(AppContext.BaseDirectory, "utils", "contraseña.json");
+            if (!File.Exists(filePath))
+                Assert.Inconclusive($"No se encontró el archivo de datos: {filePath}");
+
+            string json = File.ReadAllText(filePath);
+            JsonArray data = JsonNode.Parse(json).AsArray();
+
+            foreach (var item in data)
+            {
+                string contraseña = item?["contraseña"]?.ToString() ?? "";
+                int correcto = item?["correcto"]?.GetValue<int?>() ?? 0;
+
+                yield return new object[] { contraseña, correcto == 1 };
+            }
+        }
+
+        [TestMethod]
+        [DynamicData(nameof(ObtenerContraseñaDesdeJson))]
+        public void ContraseñaTest(string contraseña, bool esperado)
+        {
+            bool ok = u.ValidarContraseña(contraseña);
+
+            Assert.AreEqual(esperado, ok, $"Contraseña probado: {contraseña}");
+        }
+
+        [TestMethod]
+        public void CaducarCuentaTest()
+        {
+            Assert.IsTrue(u.Estado);
+
+            DateTime fechaCaducar = DateTime.Now.AddDays(-15);
+            u.FechaCaducidadCuenta = fechaCaducar;
+            u.CaducarCuenta();
+
+            Assert.IsFalse(u.Estado);
+        }
+
+        [TestMethod]
+        public void CaducarContraseñaTest()
+        {
+            Assert.IsTrue(u.Estado);
+
+            DateTime fechaCaducar = DateTime.Now.AddDays(-15);
+            u.FechaCaducidadCuenta = fechaCaducar;
+            u.CaducarContraseña();
+
+            Assert.IsFalse(u.Estado);
+            Assert.IsTrue(u.comprobarContraseña(""));
+        }
+
+        [TestMethod]
+        public void ReactivarCuentaTest()
+        {
+            DateTime fechaCaducar = DateTime.Now.AddDays(-65);
+            u.FechaCaducidadCuenta = fechaCaducar;
+            u.CaducarCuenta();
+
+            Assert.IsFalse(u.Estado);
+
+            u.ReactivarCuenta(365);
+            Assert.IsTrue(u.Estado);
+            Assert.AreEqual(DateTime.Now.AddDays(300), u.FechaCaducidadCuenta);
+
+        }
+
+        [TestMethod]
+        public void CambiarContraseñaTest()
+        {
+            Assert.IsTrue(u.ComprobarContraseña("ConMasDe12Caracteres!"));
+            u.CambiarContraseña("bZnt34T8sEb7!");
+            Assert.IsFalse(u.ComprobarContraseña("ConMasDe12Caracteres!"));
+            Assert.IsTrue(u.ComprobarContraseña("bZnt34T8sEb7!"));
+
+            DateTime fechaCaducar = DateTime.Now.AddDays(-65);
+            u.FechaCaducidadCuenta = fechaCaducar;
+            u.CaducarContraseña();
+            Assert.IsFalse(u.Estado);
+            Assert.IsTrue(u.comprobarContraseña(""));
+
+            u.CambiarContraseña("OtraContraseñaNueva22!");
+            Assert.IsTrue(u.Estado);
+            Assert.IsFalse(u.comprobarContraseña(""));
+            Assert.IsTrue(u.comprobarContraseña("OtraContraseñaNueva22!"));
+
+        }
+
+        [TestMethod]
+        public void CuentaActivaTest()
+        {
+            Assert.IsTrue(u.Estado);
+
+            u.Estado = false;
+
+            Assert.IsFalse(u.Estado);
+        }
+
+        [TestMethod]
+        public void ComprobarContraseñaTest()
+        {
+            Assert.IsTrue(u.ComprobarContraseña("ConMasDe12Caracteres!"));
+            u.CambiarContraseña("bZnt34T8sEb7!");
+            Assert.IsFalse(u.ComprobarContraseña("ConMasDe12Caracteres!"));
+            Assert.IsTrue(u.ComprobarContraseña("bZnt34T8sEb7!"));
+
+        }
+
+
+        [TestMethod()]
+        public void EqualsTest()
+        {
+            Assert.IsTrue(u.Equals(u2));
+            Assert.IsFalse(u.Equals(u3));
+
+        }
+
+        [TestMethod()]
+        public void GetHashCodeTest()
+        {
+
+            int hash1 = u.GetHashCode();
+            int hash2 = u2.GetHashCode();
+            Assert.AreEqual(hash1, hash2);
+
+            int hash3 = u3.GetHashCode();
+            Assert.AreNotEqual(hash1, hash3);
+        }
+
 
     }
 }
