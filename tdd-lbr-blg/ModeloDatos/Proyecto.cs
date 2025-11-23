5ba0544cab7411a3e0a3bd514a7e7ca2adc8130f
BLG <blg1006@alu.ubu.es>
2025-11-24 00:44:55 +0100

    Test proyecto y clase proyecto hecha

---
+++ tdd-lbr-blg/ModeloDatos/Proyecto.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+
+namespace ModeloDatos
+{
+    public class Proyecto
+    {
+        private int id;
+        private string nombre;
+        private string descripcion;
+
+
+        public Proyecto(int id, string nombre, string descripcion)
+        {
+            this.id = id;
+            this.nombre = nombre;
+            this.descripcion = descripcion;
+        }
+
+        /*GETS Y SETS*/
+        public int Id
+        {
+            get { return this.id; }
+            set { this.id = value; }
+        }
+        public string Nombre
+        {
+            get { return this.nombre; }
+            set { this.nombre = value; }
+        }
+        public string Descripcion
+        {
+            get { return this.descripcion; }
+            set { this.descripcion = value; }
+        }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Proyecto proyecto &&
+                   id == proyecto.id &&
+                   nombre == proyecto.nombre &&
+                   descripcion == proyecto.descripcion &&
+                   Id == proyecto.Id &&
+                   Nombre == proyecto.Nombre &&
+                   Descripcion == proyecto.Descripcion;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = -915643516;
+            hashCode = hashCode * -1521134295 + id.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(nombre);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(descripcion);
+            hashCode = hashCode * -1521134295 + Id.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Nombre);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Descripcion);
+            return hashCode;
+        }
+    }
+}
---
+++ tdd-lbr-blg/ModeloDatosTest/ProyectoTest.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ModeloDatos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModeloDatosTest
+{
+    [TestClass]
+    public class ProyectoTest
+    {
+        int id = 0;
+        string nombre = "Valkiria";
+        string descripcion = "Proyecto Valkiria: Aplicacion que registra actividades fisicas.";
+
+        Proyecto p = null;
+        Proyecto p2 = null;
+        Proyecto p3 = null;
+
+        [TestInitialize]
+        public void InicializaTest()
+        {
+            p = new Proyecto(id, nombre, descripcion);
+            p2 = new Proyecto(id, nombre, descripcion);
+            p3 = new Proyecto(1, "Nombre", "Proyecto de prueba");
+        }
+
+        [TestMethod]
+        public void Constructor()
+        {
+            Assert.IsNotNull(p);
+            Assert.AreEqual(id, p.Id);
+            Assert.AreEqual(nombre, p.Nombre);
+            Assert.AreEqual(descripcion, p.Descripcion);
+
+        }
+
+        [TestMethod]
+        public void GetYSetTest()
+        {
+            p.Id = 33;
+            Assert.AreEqual(33, p.Id);
+
+            p.Nombre = "Proyecto";
+            Assert.AreEqual("Proyecto", p.Nombre);
+
+            p.Descripcion = "Esto es una descripcion";
+            Assert.AreEqual("Esto es una descripcion", p.Descripcion);
+        }
+
+        [TestMethod]
+        public void EqualsTest()
+        {
+            Assert.IsTrue(p.Equals(p2));
+            Assert.IsFalse(p.Equals(p3));
+        }
+        [TestMethod]
+        public void GestHashCodeTest()
+        {
+            int hash1 = p.GetHashCode();
+            int hash2 = p2.GetHashCode();
+            Assert.AreEqual(hash1, hash2);
+
+            int hash3 = p3.GetHashCode();
+            Assert.AreNotEqual(hash1, hash3);
+        }
+
+    }
+}
